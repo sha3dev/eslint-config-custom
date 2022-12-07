4b7bad5797ed3934b0a4eec021970977ebb581c4
jc <root@sha3.dev>
2022-12-07 11:26:43 +0100

    added rule for preventing devDependencies errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ module.exports = {
   extends: ["airbnb-base", "airbnb-typescript/base", "turbo", "prettier"],
   plugins: ["prettier", "eslint-plugin-tsdoc"],
   rules: {
-    "import/prefer-default-export": "off"
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/tests/**"] }
+    ]
   },
   parserOptions: {
     project: "./tsconfig.json"
