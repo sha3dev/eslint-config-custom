6226ef794a2a3e6ad4e888e614be71fe85cc87a5
jc <root@sha3.dev>
2023-01-19 17:44:46 +0100

    support import tsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ["airbnb-base", "airbnb-typescript/base", "turbo", "prettier"],
   plugins: ["prettier", "eslint-plugin-tsdoc"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
+  },
   rules: {
     "import/prefer-default-export": "off",
     "import/no-extraneous-dependencies": [
